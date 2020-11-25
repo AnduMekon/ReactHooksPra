3dc604d053633bf45486d1e9411318b41b514d1e
AnduMekon <anduabe@yahoo.com>
2020-11-24 22:41:34 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,62 @@
 import React ,{useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
-  const [count, setCount] = useState(props.count)
-  const [text, setText] = useState('')
 
-  return (
-    <div>
-    <p> Here it is the {text || 'complite'} count: {count}</p>
-    <button onClick={()=> setCount(count + 1)}>+1</button>
-    <button onClick={()=>setCount(props.count)}>Reset</button>
-    <button onClick={()=> setCount(count -1)}>-1</button>
-    <input value={text} onChange={(e)=>setText(e.target.value)} />
-    </div>
-  )
+const NoteApp = () => {
+  const [notes, setNote] = useState([]);
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addNote =(e) => {
+    e.preventDefault()
+    setNote([ 
+      ...notes, {title}
+    ])
+    setTitle('')
+  }
+  const removeNote = (title) => {
+    setNote(notes.filter((note)=> note.title !== title))
+   
+  }
+
+    return (
+      <div>
+        <h1>Note</h1>
+        <p>{notes.map((note)=> (
+          <div key={note.title}>
+           <h3>{note.title}</h3>
+           <button onClick={()=>removeNote(note.title)}>x</button>
+          </div>
+        ))}</p>
+        <p>Add Note</p>
+        <form onSubmit={addNote}>
+          <input value={title} onChange={(e)=> setTitle(e.target.value)} />
+          <textarea>add text</textarea>
+          <button>Add note</button>
+        </form>
+      </div>
+    )
 
 }
- App.defaultProps = {
-   count:0
- }
-ReactDOM.render(<App />,document.getElementById('root'));
+
+// const App = (props) => {
+//   const [count, setCount] = useState(props.count)
+//   const [text, setText] = useState('')
+
+//   return (
+//     <div>
+//     <p> Here it is the {text || 'complite'} count: {count}</p>
+//     <button onClick={()=> setCount(count + 1)}>+1</button>
+//     <button onClick={()=>setCount(props.count)}>Reset</button>
+//     <button onClick={()=> setCount(count -1)}>-1</button>
+//     <input value={text} onChange={(e)=>setText(e.target.value)} />
+//     </div>
+//   )
+
+// }
+//  App.defaultProps = {
+//    count:0
+//  }
+ReactDOM.render(<NoteApp />,document.getElementById('root'));
 
 
